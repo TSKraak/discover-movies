4716327f6644ddd10500595c283698913a0dbf26
TSKraak <tskraak@gmail.com>
2020-10-15 12:07:33 +0200

    Added search form with functions

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
 import "./App.css";
 
+// const findMovies = (searchFor) => {
+//   console.log("Searching in parent for:", searchFor);
+// };
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">Discover Movies</header>
       <NavBar />
       <Switch>
+        {/* <Route
+          path="/discover"
+          component={() => <DiscoverMoviesPage findMovies={findMovies} />}
+        /> */}
         <Route path="/discover" component={DiscoverMoviesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function DiscoverMoviesPage() {
+  const [searchText, setSearchText] = useState(
+    localStorage.getItem("name") || ""
+  );
+  // console.log("searchText:", searchText);
+
+  const searchMovies = (event) => {
+    // console.log("What is event?", event);
+    event.preventDefault(); // in case of using a <form> this is needed to prevent refreshing
+    localStorage.setItem("name", event.target.value);
+    setSearchText(event.target.value);
+  };
+
+  const search = (text) => {
+    // If App.js would fetch the data you would use this function
+    // console.log("What is text?", text);
+    // findMovies(text);
+    localStorage.removeItem("name");
+    setSearchText("");
+  };
+
   return (
     <div>
-      <p>Discover Movies Page</p>
+      <form>
+        <label>Find movies: </label>
+        <input
+          value={searchText}
+          placeholder="Search for.."
+          onChange={searchMovies}
+        />
+        <input type="submit" value="Search" onClick={search} />
+      </form>
+      {/* <input
+        type="text"
+        placeholder="Search for.."
+        value={searchText}
+        onChange={searchMovies}
+      />
+      <button onClick={search}>Search</button>{" "} */}
     </div>
   );
 }
