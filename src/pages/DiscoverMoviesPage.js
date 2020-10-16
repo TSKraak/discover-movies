ccb6d44b7a11c03cd08cef6d6cb00c82e1b64079
TSKraak <tskraak@gmail.com>
2020-10-16 11:21:48 +0200

    Switched searchText to params.searchtext & try/err

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -10,9 +10,9 @@ export default function DiscoverMoviesPage() {
   const [searchText, setSearchText] = useState(params.searchtext || "");
   const [searchState, setSearchState] = useState("Idle");
   const [searchResult, setSearchResult] = useState("");
-  console.log("searchText:", searchText);
+  // console.log("searchText:", searchText);
 
-  console.log("WHAT IS PARAMS?", params);
+  // console.log("WHAT IS PARAMS?", params);
 
   const searchMovies = (event) => {
     // console.log("What is event?", event);
@@ -22,35 +22,40 @@ export default function DiscoverMoviesPage() {
   };
 
   useEffect(() => {
-    if (searchText === "") {
+    if (params.searchtext === undefined) {
       // if searchText is empty string, stop executing the useEffect so it won't run the first time the page is rendered
       return;
     }
+
     async function searchForMovie() {
       // console.log("Searching for:", searchText);
+      try {
+        // event.preventDefault(); // in case of using a <form> this is needed to prevent refreshing
+        setSearchState("Searching..");
+        // Best practice: encode the string so that special characters
+        //  like '&' and '?' don't accidentally mess up the URL
+        // const queryParam = encodeURIComponent(searchText);
+        const url = `https://omdbapi.com/?apikey=c9cf54c2&s=${params.searchtext}`;
 
-      // event.preventDefault(); // in case of using a <form> this is needed to prevent refreshing
-      setSearchState("Searching..");
-      // Best practice: encode the string so that special characters
-      //  like '&' and '?' don't accidentally mess up the URL
-      const queryParam = encodeURIComponent(searchText);
-      const url = `https://omdbapi.com/?apikey=c9cf54c2&s=${queryParam}`;
+        const fetchResult = await axios.get(url);
 
-      const fetchResult = await axios.get(url);
+        // console.log("What is the result?", fetchResult.data);
 
-      // console.log("What is the result?", fetchResult.data);
-
-      if (fetchResult.data.Search) {
-        // localStorage.removeItem("search");
-
-        setSearchState("Done");
-        setSearchResult(fetchResult.data.Search);
-      } else {
-        setSearchState("No results");
+        if (fetchResult.data.Search) {
+          // localStorage.removeItem("search");
+          setSearchText("");
+          setSearchState("Done");
+          setSearchResult(fetchResult.data.Search);
+        } else {
+          setSearchState("No results");
+        }
+      } catch (error) {
+        setSearchState("Something went wrong. Try again or contact the host.");
       }
     }
+
     searchForMovie();
-  }, [params]);
+  }, [params.searchtext]);
 
   const navigateToSearch = () => {
     const routeParam = encodeURIComponent(searchText);
@@ -58,6 +63,11 @@ export default function DiscoverMoviesPage() {
     setSearchText(routeParam);
   };
   // console.log("history is?", history);
+
+  const clearResults = () => {
+    history.push(`/discover/`); // to push the search term to params which is assigned useHistory and shows up after the / in url
+    setSearchText("");
+  };
 
   // const searchResultArray = [...searchResult];
   const showResults = [...searchResult].map((result) => {
@@ -95,6 +105,12 @@ export default function DiscoverMoviesPage() {
           type="submit"
           value="Search"
           onClick={navigateToSearch}
+        />
+        <input
+          className="clear-button"
+          type="submit"
+          value="Clear"
+          onClick={clearResults}
         />
       </form>
       <p className="search-status">Status: {searchState}</p>
